b10c729b4f7658d2d2ae5f06746c87d31d6a45e2
Srijan Duggal <srijanduggal17@gmail.com>
2017-09-27 21:50:01 -0400

    finished buttons for timegroup pages

--- JavaScript/timegroup.js
+++ JavaScript/timegroup.js
@@ -1,8 +1,69 @@
-//document.getElementById("settingsdiv").style.display = "none";
+var categorytoggle = 0;
+var settingstoggle = 0;
 
 function displaySettings() {
 	document.getElementById("settingsdiv").style.display = "block";
 }
 
-document.getElementById("home").addEventListener("click", function() {window.location.href="../HTML/home.html";});
 document.getElementById("settingsgear").addEventListener("click", displaySettings);
+
+function signOut() {
+	//firebase code to sign out a user
+	//if user is signed out:
+	window.location.href = "signin.html";
+}
+
+/*
+	Include something here to hide settings div if rest of page is clicked
+
+
+	function hideSettings() {
+		if (settingstoggle === 1) {
+			document.getElementById("settingsdiv").style.display = "none";
+		}
+	}
+
+	document.addEventListener("click", hideSettings);
+*/
+
+function newRow() {
+	document.getElementById("table").appendChild(document.getElementById("newrow"));
+	document.getElementById("newrow").style.display = "table-row";
+}
+
+document.getElementById("plusbutton").addEventListener("click", newRow);
+
+function toggleCategories() {
+	if (categorytoggle === 0) {
+		document.getElementById("categoryitems").style.display = "block";
+		categorytoggle = 1;
+	}
+	
+	else {
+		document.getElementById("categoryitems").style.display = "none";
+		categorytoggle = 0;
+	}
+}
+
+document.getElementById("showcategories").addEventListener("click", toggleCategories);
+
+function toggleSettings() {
+	if (settingstoggle === 0) {
+		document.getElementById("settingsdiv").style.display = "block";
+		settingstoggle = 1;
+	}
+	
+	else {
+		document.getElementById("settingsdiv").style.display = "none";
+		settingstoggle = 0;
+	}
+}
+
+document.getElementById("settingsgear").addEventListener("click", toggleSettings);
+
+function hideNewRow() {
+	document.getElementById("newrow").style.display = "none";
+	document.getElementById("categoryitems").style.display = "none";
+}
+
+document.getElementById("addbutton").addEventListener("click", hideNewRow);
