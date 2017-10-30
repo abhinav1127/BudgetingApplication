eeaedf25b85509fe3c73bde2a80d3ebb2b31e3fa
Srijan Duggal <srijanduggal17@gmail.com>
2017-10-30 00:21:36 -0400

    Signin functionality done

--- public/JavaScript/signin.js
+++ public/JavaScript/signin.js
@@ -1,68 +1,74 @@
 var database = firebase.database();
 
+document.getElementById("namerow").style.display = "none";
 document.getElementById("loginbutton").addEventListener("click", login);
 document.getElementById("signupbutton").addEventListener("click", signUp);
 
-function login() {
-	if (document.getElementById("login").style.display === "block") {
-		var email = document.getElementById("loginemail").value;
-		var password = document.getElementById("loginpassword").value;		
-		firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-			// Handle Errors here.
-			var errorCode = error.code;
-			var errorMessage = error.message;
-			if (error) {
-				console.log(errorMessage);
-			}
-		});
-
-		firebase.auth().onAuthStateChanged(function(user) {
-			if (user) {
-				var user = firebase.auth().currentUser;
-				console.log(user);
-				console.log("logged in");
-			}
-		});
-	}
-
-	else {
-		document.getElementById("login").style.display = "block";
-		document.getElementById("signup").style.display = "none";
-		console.log("change to login");
-	}
-}
-
 function signUp() {
-	if (document.getElementById("signup").style.display !== "none") {
-		var email = document.getElementById("signupemail").value;
-		var password = document.getElementById("signuppassword").value;
-		var fullname = document.getElementById("name").value;
+	if (document.getElementById("namerow").style.display !== "none") {
+		var email = document.getElementById("emailinput").value;
+		var password = document.getElementById("passwordinput").value;
+		var fullname = document.getElementById("nameinput").value;
 
-		firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-			// Handle Errors here.
-			var errorCode = error.code;
-			var errorMessage = error.message;
-			console.log(error.message);
-		});
+		var authflag = false;
 
-		firebase.auth().onAuthStateChanged(function(user) {
-			if (user) {
-				var user = firebase.auth().currentUser;
-				console.log("Account Created");
-				console.log(user);
-				console.log(user.auth);
+		if (email !== "" && password !== "") {
+			firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+				// Handle Errors here.
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				console.log(error.message);
+			});
 
-				firebase.database().ref('users/' + user.uid).set({
-					email: user.email,
-					name: fullname
-				});
-			}
-		});
+			firebase.auth().onAuthStateChanged(function(user) {
+				if (user) {
+					var user = firebase.auth().currentUser;
+
+					if (authflag === false) {
+						console.log("Account Created");
+						firebase.database().ref('users/' + user.uid).set({
+							email: user.email,
+							name: fullname
+						});
+						console.log("Data pushed");
+						authflag = true;
+					}
+				}
+			});
+		}
 	}
 
 	else {
-		document.getElementById("signup").style.display = "block";
-		document.getElementById("login").style.display = "none";
+		document.getElementById("namerow").style.display = "table-row";
 		console.log("change to sign in");
 	}	
+}
+
+function login() {
+	if (document.getElementById("namerow").style.display === "none") {
+		var email = document.getElementById("emailinput").value;
+		var password = document.getElementById("passwordinput").value;	
+
+		if (email !== "" && password !== "") {
+			firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+				// Handle Errors here.
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				console.log(errorMessage);
+			});
+
+			firebase.auth().onAuthStateChanged(function(user) {
+				if (user) {
+					var user = firebase.auth().currentUser;
+					console.log(user);
+					console.log("logged in");
+				}
+			});	
+		}	
+	}
+
+	else {
+		document.getElementById("namerow").style.display = "table-row";
+		console.log("change to login");
+	}
 }
